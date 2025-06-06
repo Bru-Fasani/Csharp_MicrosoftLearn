f5cd34437a89453559342a949ef5c6cd2f2d2ed5
Bru-Fasani <brunafasani.arts@gmail.com>
2025-06-06 19:52:13 -0300

    Contador.cs

---
+++ Contador.cs
@@ -0,0 +1,12 @@
+int contador = 0;
+
+while (contador>10){
+  contador ++;
+
+  if (contador == 4){
+    continue;
+  }
+  Console.WriteLine("==>"+ contador);
+}
+
+Console.WriteLine("Fim do laço");
