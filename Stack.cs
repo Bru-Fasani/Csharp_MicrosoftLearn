a9367e9b2ec4090e93e75da3102abf04f8ab8829
Bru-Fasani <brunafasani.arts@gmail.com>
2025-07-02 15:18:53 -0300

    Stack.cs

---
+++ Stack.cs
@@ -0,0 +1,19 @@
+Stack<int> Pilha = new Stack<int>();
+
+Pilha.Push(2);
+Pilha.Push(3);
+Pilha.Push(4);
+Pilha.Push(5);
+
+foreach (var item in Pilha)
+{
+    Console.WriteLine(item.ToString());
+}
+
+//Remove o último item a Pilha
+Pilha.Pop();
+
+foreach (var item in Pilha)
+{
+    Console.WriteLine(item.ToString());
+}
