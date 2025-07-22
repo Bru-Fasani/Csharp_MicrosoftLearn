bae2135f2432969eadef58c71f6096c462b06fe5
Bru-Fasani <brunafasani.arts@gmail.com>
2025-07-22 14:34:04 -0300

    Create IndexOf e Substring.cs

---
+++ IndexOf e Substring.cs
@@ -0,0 +1,15 @@
+string message = "What is the value <span>between the tags</span>?";
+
+const string openSpan = "<span>";
+const string closeSpan = "</span>";
+ 
+int openingPosition = message.IndexOf(openSpan);
+int closingPosition = message.IndexOf(closeSpan);
+
+//Console.WriteLine(openingPosition);
+//Console.WriteLine(closingPosition);
+
+openingPosition += openSpan.Length;
+
+int length = closingPosition - openingPosition;
+Console.WriteLine(message.Substring(openingPosition, length));
